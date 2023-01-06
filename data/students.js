3e40cc16d17af1b7fd0491ef7effd61d3065c031
hpweisberg <hpweisberg@gmail.com>
2023-01-06 10:06:18 -0800

    add updates

---
+++ data/students.js
@@ -0,0 +1,26 @@
+const students = [
+  {first: 'Brandon', last: 'Hickman'},
+  {first: 'Bryce', last: 'Cazier'},
+  {first: 'Catherine', last: 'Matson'},
+  {first: 'Charlie', last: 'Mickey'},
+  {first: 'Chris', last: 'Campbell'},
+  {first: 'Dimi', last: 'Savva'},
+  {first: 'Harrison', last: 'Weisberg'},
+  {first: 'Jake', last: 'Todoroki'},
+  {first: 'Kendra', last: 'Lacey'},
+  {first: 'Manny', last: 'Sanchez'},
+  {first: 'Pablo', last: 'Villeda-Echeverria'},
+  {first: 'Yan', last: 'Shi'},
+  {first: 'Yong', last: 'Park'},
+]
+
+const teachers = [
+  {first: 'Angel', last: 'Pichardo'},
+  {first: 'Ben', last: 'Manley'},
+  {first: 'Tom', last: 'Kolsrud'},
+]
+
+export {
+  students,
+  teachers
+}
